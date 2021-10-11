69b719a8ebac8ca3dc293d2443c838578a05b087
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-12 01:54:46 +0600

    Added firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI4GJOPb4LIGVISIFKxiF3qqhtoNXkYAE",
+    authDomain: "react-auth-integration-76889.firebaseapp.com",
+    projectId: "react-auth-integration-76889",
+    storageBucket: "react-auth-integration-76889.appspot.com",
+    messagingSenderId: "903738885652",
+    appId: "1:903738885652:web:7596b36151d83d69be0901"
+  };
+
+export default firebaseConfig;
---
+++ src/firebase/firebaseInitialize.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
+
+
+const initAuthentication = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default initAuthentication;
